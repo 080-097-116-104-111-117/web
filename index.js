c5d43b0fe823ff7fdb1876c7bcb1c2532e7007a8
Pathou <mingselmrk@gmail.com>
2021-05-08 23:30:16 +0530

    added valitation in the form

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 
-function myFunction(e){
+function myFunction(e) {
     let loanAmount = document.getElementById("amount").value;
     let loanInterest = document.getElementById("apr").value;
     let loanduration = document.getElementById("duration").value;
 
-    
 
-    let totalInterest = (loanAmount*loanInterest*loanduration)/100;
+
+    let totalInterest = (loanAmount * loanInterest * loanduration) / 100;
     let totalAmount = parseInt(loanAmount) + parseInt(totalInterest);
 
 
     let EMI = Math.round(totalAmount / loanduration);
 
-    document.getElementById("text").innerHTML ="<B> EMI: Rs. <B> " + EMI + "<br>" +  "<B> Total: Rs. <B>" + totalAmount;
-    document.getElementById("result").style.display ="block";
-    console.log(totalInterest);
+    document.getElementById("text").innerHTML = "<B> EMI: Rs. <B> " + EMI + "<br>" + "<B> Total: Rs. <B>" + totalAmount;
+
+    
+    if (loanAmount === "" || loanInterest === "" || loanduration === "") {
+        // alert("please fill!");
+        
+    }
+    else {
+        document.getElementById("result").style.display = "block";
+        document.getElementById("myForm").reset();
+    }
+        
+    
     e.preventDefault();
-    document.getElementById("myForm").reset();
+    
 }
 
-document.getElementById("bttn").addEventListener("click",myFunction);
-document.getElementById("exit").addEventListener("click",hide);
+document.getElementById("bttn").addEventListener("click", myFunction);
+document.getElementById("exit").addEventListener("click", hide);
 
 function hide() {
     document.getElementById("result").style.display = "none";
