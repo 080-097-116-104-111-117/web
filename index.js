a2db0d98ada5fd7ecbe564288572f59ca30a4762
Pathou <mingselmrk@gmail.com>
2021-05-09 14:56:55 +0530

    adding for mobile UI

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 
 function myFunction(e) {
+    // take input 
     let loanAmount = document.getElementById("amount").value;
     let loanInterest = document.getElementById("apr").value;
     let loanduration = document.getElementById("duration").value;
 
-
-
-    let totalInterest = (loanAmount * loanInterest * loanduration) / 100;
-    let totalAmount = parseInt(loanAmount) + parseInt(totalInterest);
-
-
+    // calculation
+    let totalInterest = parseFloat(loanAmount * loanInterest * loanduration) / 100;
+    let totalAmount = parseInt(loanAmount) + parseFloat(totalInterest);
+    totalAmount = Math.round(totalAmount);
     let EMI = Math.round(totalAmount / loanduration);
 
-    document.getElementById("text").innerHTML = "<B> EMI: Rs. <B> " + EMI + "<br>" + "<B> Total: Rs. <B>" + totalAmount;
-
-    
+    // validation
     if (loanAmount !== "" || loanInterest !== "" || loanduration !== "") {
         document.getElementById("result").style.display = "block";
         document.getElementById("myForm").reset();
@@ -26,6 +23,7 @@ function myFunction(e) {
         
     }
     
+    document.getElementById("text").innerHTML = "<B> EMI: Rs. <B> " + EMI + "<br>" + "<B> Total: Rs. <B>" + totalAmount;
     e.preventDefault();
 }
 
